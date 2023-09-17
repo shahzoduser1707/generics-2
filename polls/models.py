5a2e7cca864fc329ed37e63ee958aa915e881836
Shahzod <tohirovshahzod1707@gmail.com>
2023-09-18 01:34:00 +0500

    only get permission added

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Workers(models.Model):
     worker_name = models.CharField(max_length=200,default='')
     worker_birthday = models.DateTimeField(default=datetime.now)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
     class Meta:
         db_table = 'Workers'
     def __str__(self) -> str:
--- polls/views.py
+++ polls/views.py
@@ -3,11 +3,13 @@ from .models import Workers,Orders
 from .serializer import WorkersSerializers,OrdersSerializers
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class getWorker(generics.ListCreateAPIView):
     queryset = Workers.objects.all()
     serializer_class = WorkersSerializers
+    permission_classes = (IsAuthenticated,)
 
 class DetailDestroyUpdateView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Orders.objects.all()
